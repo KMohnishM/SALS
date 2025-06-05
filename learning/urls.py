e4522cecb58abaa308b69ba0fdee6efcded4187b
Mohnish <kmohnishm@gmail.com>
2025-06-05 11:39:06 +0530

    Reset

--- learning/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('generate-quiz/', views.generate_quiz, name='generate_quiz'),
-    path('analyze-quiz/', views.analyze_quiz, name='analyze_quiz'),
-    path('learning-path/', views.learning_path, name='learning_path'),
-    path('final-quiz/', views.final_quiz, name='final_quiz'),
-]
